7a5bf53ff899df0adc28f4fa022495b8d626672f
wuuconix <wuuconix@qq.com>
2023-01-26 15:08:23 +0800

    a a way for 70-climbing-stairs.ts and add 322-coin-change.ts

--- carl/DynamicProgramming/70-climbing-stairs.ts
+++ carl/DynamicProgramming/70-climbing-stairs.ts
@@ -1,4 +1,4 @@
-function climbStairs(n: number): number {
+function climbStairs1(n: number): number {
   const dp = new Array(n)
   dp[0] = 1
   dp[1] = 2
@@ -8,5 +8,19 @@ function climbStairs(n: number): number {
   return dp[n - 1]
 }
 
+// using the think way of backpack problem
+function climbStairs(n: number): number {
+  const dp: number[] = new Array(n + 1).fill(0)
+  dp[0] = 1
+  dp[1] = 1
+  for (let i = 2; i <= n; i++) {
+    for (let j = 1; j <= 2; j++) {
+      dp[i] += dp[i - j]
+    }
+  }
+  console.log(dp)
+  return dp[n]
+}
+
 const n = 3
-console.log(climbStairs(45))
+console.log(climbStairs(n))
