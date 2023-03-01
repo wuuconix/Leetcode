78bc673b683cbef8a38721f1c1e668f0dcf0cd88
wuuconix <wuuconix@qq.com>
2023-03-01 10:18:32 +0800

    add 53-maximum-subarray.ts

---
+++ push.ps1
@@ -0,0 +1,8 @@
+$message = $args[0]
+Write-Host $message
+if (Test-Path Alias:curl) { Remove-Item Alias:curl }
+curl "https://leetcard.jacoblin.cool/wuuconix?theme=unicorn&font=Electrolize&ext=heatmap&site=cn" -o test.svg
+Write-Host "svg curl ok"
+git add .
+git commit -m "$message"
+git push
