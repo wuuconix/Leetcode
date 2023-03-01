e8e9295a38873a6a3576045ad9320f441eb903fe
wuuconix <wuuconix@qq.com>
2023-03-01 10:19:58 +0800

    fix a bug in push.ps1

--- push.ps1
+++ push.ps1
@@ -1,7 +1,7 @@
 $message = $args[0]
 Write-Host $message
 if (Test-Path Alias:curl) { Remove-Item Alias:curl }
-curl "https://leetcard.jacoblin.cool/wuuconix?theme=unicorn&font=Electrolize&ext=heatmap&site=cn" -o test.svg
+curl "https://leetcard.jacoblin.cool/wuuconix?theme=unicorn&font=Electrolize&ext=heatmap&site=cn" -o stats.svg
 Write-Host "svg curl ok"
 git add .
 git commit -m "$message"
