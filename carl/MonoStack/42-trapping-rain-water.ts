ab01bdc20c65f2fb87cbc173086f5d2673aa4a0f
wuuconix <wuuconix@qq.com>
2023-03-18 11:40:46 +0800

    fix 42-trapping-rain-water.ts

--- carl/MonoStack/42-trapping-rain-water.ts
+++ carl/MonoStack/42-trapping-rain-water.ts
@@ -1,23 +1,28 @@
 function trap(height: number[]): number {
-  while (height[0] == 0) {
-    height.shift()
-  }
   const monoStack: number[] = []
   let ans = 0
-  for (let i = 0; i < height.length; i++) {
-    let left: number = i
-    let min: number = Infinity
-    while (monoStack.length != 0 && height[monoStack[monoStack.length - 1]] <= height[i]) {
-      left = monoStack.pop()!
-      min = Math.min(min, height[left])
+  monoStack.push(0)
+  for (let i = 1; i < height.length; i++) {
+    if (height[i] < height[monoStack[monoStack.length - 1]]) {
+      monoStack.push(i)
+    } else if (height[i] == height[monoStack[monoStack.length - 1]]) {
+      monoStack.pop()
+      monoStack.push(i)
+    } else {
+      while (monoStack.length != 0 && height[i] > height[monoStack[monoStack.length - 1]]) {
+        const mid = monoStack.pop()!
+        if (monoStack.length != 0) {
+          const left = monoStack[monoStack.length - 1]
+          const h = Math.min(height[left], height[i]) - height[mid]
+          const w = i - left - 1
+          ans += h * w
+        }
+      }
+      monoStack.push(i)
     }
-    for (let j = left + 1; j < i; j++) {
-      ans += (height[left] - Math.max(height[j], min))
-    }
-    monoStack.push(i)
   }
   return ans
 }
 
-const height = [0,1,0,2,1,0,1,3,2,1,2,1]
+const height = [4,2,3]
 console.log(trap(height))
